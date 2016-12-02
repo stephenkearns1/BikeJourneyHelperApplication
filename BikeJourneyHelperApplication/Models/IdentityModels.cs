5bfc73df8279a6ecf29c5a5b1f2263f6d83cc16b
stephenkearns1 <bohsbabycrew1@gmail.com>
2016-12-02 17:23:01 +0000

    Styling site and fixing bugs

--- BikeJourneyHelperApplication/Models/IdentityModels.cs
+++ BikeJourneyHelperApplication/Models/IdentityModels.cs
@@ -6,9 +6,13 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace BikeJourneyHelperApplication.Models
 {
+   
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
+        //Holds the users 
+        public string Name { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
