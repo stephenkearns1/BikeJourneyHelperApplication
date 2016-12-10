700e2842ea8bfedbbd547510b90f75916a1939b3
stephenkearns1 <bohsbabycrew1@gmail.com>
2016-12-10 14:34:44 +0000

    Added WebApi2 and updated notes to use the api

---
+++ BikeJourneyHelperApplication/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace BikeJourneyHelperApplication.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.MapHttpAttributeRoutes();
+            configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+            //Returns json instead of xml, to make the parsing easier in client side javascripts
+            var appXmlType = configuration.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            configuration.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+        }
+    }
+}
---
+++ BikeJourneyHelperApplication/WebAPI/NotesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BikeJourneyHelperApplication.DAL;
+using BikeJourneyHelperApplication.Models;
+
+namespace BikeJourneyHelperApplication.WebAPI
+{
+    public class NotesController : ApiController
+    {
+        private BikeJourneyHelperContext db = new BikeJourneyHelperContext();
+        [Authorize]
+        // GET: api/Notes
+        public IQueryable<Note> GetNotes()
+        {
+            return db.Notes;
+        }
+       
+        // GET: api/Notes/5
+        [ResponseType(typeof(Note))]
+        public IHttpActionResult GetNote(int id)
+        {
+            Note note = db.Notes.Find(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(note);
+        }
+
+        // PUT: api/Notes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutNote(int id, Note note)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != note.NoteID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(note).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NoteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Notes
+        [ResponseType(typeof(Note))]
+        public IHttpActionResult PostNote(Note note)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Notes.Add(note);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = note.NoteID }, note);
+        }
+
+        // DELETE: api/Notes/5
+        [ResponseType(typeof(Note))]
+        public IHttpActionResult DeleteNote(int id)
+        {
+            Note note = db.Notes.Find(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            db.Notes.Remove(note);
+            db.SaveChanges();
+
+            return Ok(note);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool NoteExists(int id)
+        {
+            return db.Notes.Count(e => e.NoteID == id) > 0;
+        }
+    }
+}
